ac78317b5788ef6057f4d2d136542ff3c21338b9
RODRIGO GREGORIO NERI <rodrigogregorioneri@hotmail.com>
2021-06-12 12:07:27 -0300

    alterando versão java

--- src/main/java/neri/rodrigo/botmsd/repository/EstoqueRepository.java
+++ src/main/java/neri/rodrigo/botmsd/repository/EstoqueRepository.java
@@ -1,16 +1,18 @@
 package neri.rodrigo.botmsd.repository;
 
 import neri.rodrigo.botmsd.model.EstoqueModel;
-import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.JpaRepository;
 //import org.springframework.data.jpa.repository.Query;
 //import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface EstoqueRepository extends JpaRepository<EstoqueModel, Integer> {
+public interface EstoqueRepository {
 
 
 //    @Query(name="select * from TB_Estoque where marca = 'AFTOSA'", nativeQuery = true)
 //    List<EstoqueModel> findAllMarcas(String marca);
+
+    //extends JpaRepository<EstoqueModel, Integer>
 
 }
