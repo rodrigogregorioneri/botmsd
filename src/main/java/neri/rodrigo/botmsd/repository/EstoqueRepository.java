2d128a9d155bb26f1ce27929f57b192560aaba17
RODRIGO GREGORIO NERI <rodrigogregorioneri@hotmail.com>
2021-06-12 12:23:22 -0300

    revertendo

--- src/main/java/neri/rodrigo/botmsd/repository/EstoqueRepository.java
+++ src/main/java/neri/rodrigo/botmsd/repository/EstoqueRepository.java
@@ -1,18 +1,16 @@
 package neri.rodrigo.botmsd.repository;
 
 import neri.rodrigo.botmsd.model.EstoqueModel;
-//import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 //import org.springframework.data.jpa.repository.Query;
 //import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface EstoqueRepository {
+public interface EstoqueRepository extends JpaRepository<EstoqueModel, Integer> {
 
 
 //    @Query(name="select * from TB_Estoque where marca = 'AFTOSA'", nativeQuery = true)
 //    List<EstoqueModel> findAllMarcas(String marca);
-
-    //extends JpaRepository<EstoqueModel, Integer>
 
 }
