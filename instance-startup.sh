4986157b7deae379503382874d314656f2c9e466
RODRIGO GREGORIO NERI <rodrigogregorioneri@hotmail.com>
2021-06-21 13:04:36 -0300

    revertendo

---
+++ instance-startup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Set the metadata server to the get projct id
+PROJECTID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
+BUCKET=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/BUCKET" -H "Metadata-Flavor: Google")
+
+echo "Project ID: ${PROJECTID} Bucket: ${BUCKET}"
+
+# Get the files we need
+gsutil cp gs://${BUCKET}/botmsd-0.0.1-SNAPSHOT.jar .
+
+# Install dependencies
+apt-get update
+apt-get -y --force-yes install openjdk-8-jdk
+
+# Make Java 8 default
+update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+
+# Start server
+java -jar botmsd-0.0.1-SNAPSHOT.jar
