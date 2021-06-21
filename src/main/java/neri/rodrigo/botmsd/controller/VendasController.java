c76c317d516c20e3c800aa962754fd3cb56fd386
RODRIGO GREGORIO NERI <rodrigogregorioneri@hotmail.com>
2021-06-21 18:44:39 -0300

    revertendo

--- src/main/java/neri/rodrigo/botmsd/controller/VendasController.java
+++ src/main/java/neri/rodrigo/botmsd/controller/VendasController.java
@@ -47,32 +47,48 @@ public class VendasController {
 //    }
 
     @PostMapping("/realizadofamiliaessemes")
-    public Response getRealizadoFamiliaEsseMes(@RequestBody Request request){
+    public Response getRealizadoFamiliaEsseMes(@RequestBody Request request) {
         return vendasBusiness.getRealizadoFamiliaEsseMes(request);
     }
 
     @PostMapping("/desempenhodevendasdogerenteyparaprodutox")
-    public Response getDesempenhoDeVendasDoGerenteYparaProdutoX(@RequestBody Request request){
+    public Response getDesempenhoDeVendasDoGerenteYparaProdutoX(@RequestBody Request request) {
         return vendasBusiness.getDesempenhoDeVendasDoGerenteYparaProdutoX(request);
     }
 
     @PostMapping("/realizadodocolaboradorxessemes")
-    public Response getRealizadoDoColaboradorXesseMes(@RequestBody Request request){
+    public Response getRealizadoDoColaboradorXesseMes(@RequestBody Request request) {
         return vendasBusiness.getRealizadoDoColaboradorXesseMes(request);
     }
 
     @PostMapping("/quantidadedevendastotalparacadacliente")
-    public Response getQuantidadeDeVendasTotalParaCadaCliente(@RequestBody Request request){
+    public Response getQuantidadeDeVendasTotalParaCadaCliente(@RequestBody Request request) {
         return vendasBusiness.getQuantidadeDeVendasTotalParaCadaCliente(request);
     }
 
     @PostMapping("/somadevendasrealizadasparacadaclienteseumamesmaregional")
-    public Response getSomaDeVendasRealizadasParaCadaClienteSeUmaMesmaRegional(@RequestBody Request request){
+    public Response getSomaDeVendasRealizadasParaCadaClienteSeUmaMesmaRegional(@RequestBody Request request) {
         return vendasBusiness.getSomaDeVendasRealizadasParaCadaClienteSeUmaMesmaRegional(request);
     }
 
+    @PostMapping("/itentCinco")
+    public Response getItent5(@RequestBody Request request) {
+        return vendasBusiness.getItent5(request);
+    }
 
+    @PostMapping("/itentSeis")
+    public Response getItent6(@RequestBody Request request) {
+        return vendasBusiness.getItent6(request);
+    }
 
+    @PostMapping("/itentNove")
+    public Response getItent9(@RequestBody Request request) {
+        return vendasBusiness.getItent9(request);
+    }
 
+    @PostMapping("/itentDez")
+    public Response getItent10(@RequestBody JsonNode request) {
+        return vendasBusiness.getItent10(request);
+    }
 
 }
